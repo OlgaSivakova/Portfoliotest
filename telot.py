f9038c5335469d7dc9f75a1b357acc96315bd6ac
Olga Sivakova <117565749+OlgaSivakova@users.noreply.github.com>
2023-09-05 13:44:07 +0300

    testbotwithusersinputtime

--- telot.py
+++ telot.py
@@ -1,8 +1,42 @@
 import telebot as p
 import wikipedia
+import pickle
+import os
 wikipedia.set_lang('ru')
 
 bot = p.TeleBot('6014014781:AAHxcPLpkigJJ6CQc8L--VRB2iXdo9_Gk8I')
+d ={'12.04': ['12:00','13:00','16:00'], '13.04': ['12:00','13:00','16:00'] }
+nonemptyfile = 'my_dict.pkl'
+
+def book(dic, n ,m):
+
+    if os.path.getsize(nonemptyfile)==0:
+
+        if m in dic[n]:
+                ind = dic[n].index(m)
+                dic[n].pop(ind)
+                dirs = {n : dic[n]}
+                dic.update( dirs)
+
+        with open('my_dict.pkl', 'wb') as f:
+            pickle.dump(dic, f)
+        return dic
+    elif os.path.getsize(nonemptyfile)!=0:
+        with open('my_dict.pkl', 'rb') as f:
+            ld = pickle.load(f)
+       
+
+        if m in ld[n]:
+                ind = ld[n].index(m)
+                ld[n].pop(ind)
+                dirs = {n : ld[n]}
+                ld.update( dirs)
+        with open('my_dict.pkl', 'wb') as f:
+            pickle.dump(ld, f)
+        return ld  
+
+
+
 
 @bot.message_handler(commands=['start'])#команда с которой нужно начинать
 def welcom(message):
@@ -14,6 +48,16 @@ def talk(message):
         bot.send_message(message.chat.id, 'И тебе привет')
     elif message.text=='Получить информацию':
         bot.send_message(message.chat.id, 'Введите название запроса')
+    elif message.text=='Запись':
+        bot.send_message(message.chat.id, a)
+    elif message.text=='13.04/16:00':
+        a = message.text.split('/')[0]
+        b = message.text.split('/')[1]
+        user = str(book(d, a, b))
+
+        bot.send_message(message.chat.id,  user )
+        
+      
     else:
         mes = message.text
         mew = mes.replace(' ', '_')
