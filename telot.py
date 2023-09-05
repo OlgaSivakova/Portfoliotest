aeb6705f088810f5072fa09e0b1d46bbc7e8c55d
Olga Sivakova <117565749+OlgaSivakova@users.noreply.github.com>
2023-09-05 16:01:16 +0300

    telebotrightway

--- telot.py
+++ telot.py
@@ -2,6 +2,17 @@ import telebot as p
 import wikipedia
 import pickle
 import os
+from moviepy.editor import*
+from pathlib import Path
+whattodo = ''
+def inputvideotocreate(nameoffile):
+     vidsec = f'{nameoffile}.mp4'
+     path1=Path(vidsec)
+     return(path1)
+dictoptions = {0: 'Выход из программы', 1: 'Склеить два видео',2: 'Извлечь музыку из видео', 3 : 'Обрезать видео', 4 : 'Преобразовать видео в gif'}
+
+    
+        
 wikipedia.set_lang('ru')
 
 bot = p.TeleBot('6014014781:AAHxcPLpkigJJ6CQc8L--VRB2iXdo9_Gk8I')
@@ -34,35 +45,47 @@ def book(dic, n ,m):
         with open('my_dict.pkl', 'wb') as f:
             pickle.dump(ld, f)
         return ld  
+#if os.path.getsize(nonemptyfile)==0:#
+#            a = str(input(f'Выберите дату из свежего списка{d.keys()}'))
+#            b = str(input(f'Выберите время из свежего списка{d[a]}'))
+ #           d = d
+#if os.path.getsize(nonemptyfile)!=0:
+#            with open('my_dict.pkl', 'rb') as f:
+#                    ld = pickle.load(f)
+#            a = str(input(f'Выберите дату из использованного списка{ld.keys()}'))
+#            b = str(input(f'Выберите время из использованного списка{ld[a]}'))
+#            user = str(book(d, a, b))
 
-if os.path.getsize(nonemptyfile)==0:
-    a = str(input(f'Выберите дату из свежего списка{d.keys()}'))
-    b = str(input(f'Выберите время из свежего списка{d[a]}'))
-    d = d
-if os.path.getsize(nonemptyfile)!=0:
-    with open('my_dict.pkl', 'rb') as f:
-            ld = pickle.load(f)
-    a = str(input(f'Выберите дату из использованного списка{ld.keys()}'))
-    b = str(input(f'Выберите время из использованного списка{ld[a]}'))
-    
-
-user = str(book(d, a, b))
 
 
 @bot.message_handler(commands=['start'])#команда с которой нужно начинать
 def welcom(message):
-    bot.send_message(message.chat.id, 'Давай пообщаемся!')
+    bot.send_message(message.chat.id, f'Выберите действие из списка {dictoptions.keys()}')
     
 @bot.message_handler(content_types=['text'])#значение которое будем принимать
 def talk(message):
-    if message.text=='Привет':
-        bot.send_message(message.chat.id, 'И тебе привет')
+    if message.text=='1':
+        bot.send_message(message.chat.id, 'Введите название двух видео')
+    elif '|' in message.text:
+        firstvideo = message.text.split('|')[0]
+        secondvideo = message.text.split('|')[1]
+        path1=inputvideotocreate(firstvideo)
+        path2=inputvideotocreate(secondvideo)
+        video = VideoFileClip(f'{path1}')
+            
+        video2 = VideoFileClip(f'{path2}')
+        got = concatenate_videoclips([video, video2])
+        got.write_videofile('Final.mp4')
+        bot.send_message(message.chat.id, 'Final.mp4')
     elif message.text=='Получить информацию':
         bot.send_message(message.chat.id, 'Введите название запроса')
     elif message.text=='Запись':
         bot.send_message(message.chat.id, a)
-    elif message.text=='Запись времени':
-        
+    elif '.' in message.text and '/' in message.text and ':' in message.text and len(message.text)==11:
+        a = message.text.split('/')[0]
+        b = message.text.split('/')[1]
+        user = str(book(d, a, b))
+
         bot.send_message(message.chat.id,  user )
         
       
